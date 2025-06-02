93cf5ab3699a8791b928d6d3d79c739594c69cbb
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-06-02 23:03:27 +0200

    Add moving rectangle example

--- Program.cs
+++ Program.cs
@@ -1,22 +1,42 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
 
 namespace RaylibCSharpWorkshop;
 
 class Program
 {
+    private const int WindowWidth = 800;
+    private const int WindowHeight = 600;
     [STAThread]
     public static void Main()
     {
-        Raylib.InitWindow(800, 680, "Hello World");
+        InitWindow(WindowWidth, WindowHeight, "Hello World");
 
-        while (!Raylib.WindowShouldClose())
+        SetTargetFPS(60);
+
+        var rectanglePosition = new Vector2(WindowWidth / 2, WindowHeight / 2);
+        var rectangleSize = new Vector2(25, 25);
+        var rectangleSpeed = new Vector2(2, 0);
+
+        while (!WindowShouldClose())
         {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.White);
-            Raylib.DrawText("Hello world!", 12, 12, 20, Color.Red);
-            Raylib.EndDrawing();
+            if (rectanglePosition.X + rectangleSize.X >= GetScreenWidth())
+            {
+                rectanglePosition.X = 0;
+            }
+            else
+            {
+                rectanglePosition.X += rectangleSpeed.X;
+                rectanglePosition.Y += rectangleSpeed.Y;      
+            }
+
+            BeginDrawing();
+            ClearBackground(Color.White);
+            DrawRectangleV(rectanglePosition, rectangleSize, Color.Blue);
+            EndDrawing();
         }
 
-        Raylib.CloseWindow();
+        CloseWindow();
     }
 }
