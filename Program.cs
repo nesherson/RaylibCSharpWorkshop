04815ac214c6b097c28d7c1940cdf68f78e078d8
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-06-02 22:38:10 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Raylib_cs;
+
+namespace RaylibCSharpWorkshop;
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 680, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            Raylib.DrawText("Hello world!", 12, 12, 20, Color.Red);
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
